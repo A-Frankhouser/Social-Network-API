69a667b6c08d35dc72828704d77d37b2428633d3
Alex Frankhouser <freesoul423@gmail.com>
2023-01-29 15:55:45 -0500

    added User and Thoughts to models folder

---
+++ models/Thoughts.js
@@ -0,0 +1,72 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+const { moveMessagePortToContext } = require('worker_threads');
+
+// reaction sub-document
+const ReactionSchema = new Schema(
+    {
+        reactionID: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+    },
+    {
+        toJSON: {
+            virtual: true,
+            getters
+        },
+        id: false,
+    }
+);
+
+// Thought Schema
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) =>
+            moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtual: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought; 
