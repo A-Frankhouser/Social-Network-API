3a720361e7c5289b9e9d65511a06c61aff226c02
Alex Frankhouser <freesoul423@gmail.com>
2023-01-29 13:28:01 -0500

    setup the server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const exp = require('constants');
+const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./config/connection');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API sever is running on ${PORT}`);
+    })
+})
