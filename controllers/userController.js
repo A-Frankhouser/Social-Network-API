92720ddf472662d2649c36aff8ad948b5f1859c2
Alex Frankhouser <freesoul423@gmail.com>
2023-01-30 18:18:39 -0500

    started working on userController.js. Also debugged my Thought model

---
+++ controllers/userController.js
@@ -0,0 +1,58 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // Gets all Users
+    getUser(req, res) {
+        User.find({})
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err));
+    },
+    
+    // Gets a single User
+    getSingleUser(req, res) {
+        User.findOne({_id: req.params.userId})
+        .populate({ path: 'thoughts', select: '-__v' })
+        .populate({ path: 'friends', select: '-__v' })
+        .select('-__v')
+        .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'Sorry, no User found with that ID!'})
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // Create a User
+    createUser(req,res) {
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    },
+    // Update a User
+    updateUser(req,res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) => 
+                !user
+                    ? res.status(404).json({ message: 'Sorry, no User found with this ID!'})
+                    : res.json(user)
+                )
+                .catch((err) => res.status(500).json(err));
+    },
+    // Delete a User
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) => 
+                !user
+                    ? res.status(404).json({ message: 'Sorry, no User found with this ID!'})
+                    : Thought.deleteMany({ _id: { $in: user.thoughts }})
+                )
+                .then(() => res.json({ message: 'The User and Thought was deleted :D'}))
+                .catch((err) => res.status(500).json(err));
+    }
+}
