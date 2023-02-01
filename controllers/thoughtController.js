813c1b21b40c37abb87240b509208274ee6ce6e6
Alex Frankhouser <freesoul423@gmail.com>
2023-02-01 15:39:39 -0500

    started working on createReaction in thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -52,5 +52,40 @@ module.exports = {
                 : res.json(user)
             )
             .catch((err) => res.status(500).json(err));
+    },
+
+    // Delete a thought.
+    deleteThought(req,res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "Sorry, no thought found with this id:/ " })
+                    : User.findOneAndUpdate(
+                        { thoughts: req.params.thoughtId },
+                        { $pull: { thoughts: req.params.thoughtId } },
+                        { new: true }
+                    )
+            )
+            .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'Sorry! The thought was deleted, but no user was found:/' })
+                : res.json({ message: "The thought was deleted 🥳 " })
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // Create a reaction.
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true}
+        )
+        .then((thought) =>
+            !thought
+                ? res.status(404).json({ message: 'Sorry, no thought found with this id:/ '})
+                : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
     }
 }
